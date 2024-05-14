de2d7c7e3120be62f972386a651a35c094d44aca
Meadow <meadowingc@gmail.com>
2024-05-14 17:18:56 -0600

    add openai library so we can more easily stream completions

--- src/lib/OpenAIGPT4VisionService.ts
+++ src/lib/OpenAIGPT4VisionService.ts
@@ -1,4 +1,8 @@
-import { writable, get } from "svelte/store";
+import OpenAI, { AzureOpenAI } from "openai";
+import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import type { ChatCompletionChunk } from "openai/src/resources/index.js";
+import type { Stream } from "openai/streaming.mjs";
+import { get, writable, type Writable } from "svelte/store";
 import { toastMessage } from "./toastService";
 
 const openAiApiKey = writable<string>("");
@@ -9,15 +13,17 @@ const azureOpenAiDeployment = writable<string>("");
 const azureOpenAiKey = writable<string>("");
 const azureOpenAiVersion = writable<string>("");
 
-async function correctText(ocrText: string, image: Blob): Promise<string> {
+async function correctText(
+  ocrText: string,
+  image: Blob,
+  onStreamTokenCollaback: (currentText: string, finished: boolean) => void
+) {
   const base64Image = await blobToBase64(image);
 
-  const payload = {
-    model: get(useAzureOpenAI) ? get(azureOpenAiDeployment) : "gpt-4-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `
+  const messages: Array<ChatCompletionMessageParam> = [
+    {
+      role: "system",
+      content: `
 You are an advanced AI trained to correct errors in text extracted from images of handwritten documents. The user will provide you two inputs:
 
 1. The raw text of that image as extracted by an OCR (Optical Character Recognition) process.
@@ -29,28 +35,25 @@ The OCR process will often output incorrect text so it's up to you to correct a 
 
 Please output the corrected text in a clean and readable format, ready for use. Please represent as proper markdown any text formatting that you see in the picture: paragraph breaks, lists, and quotes, etc.
 `,
-      },
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text: ocrText,
+    },
+    {
+      role: "user",
+      content: [
+        {
+          type: "text",
+          text: ocrText,
+        },
+        {
+          type: "image_url",
+          image_url: {
+            url: `data:image/jpeg;base64,${base64Image}`,
           },
-          {
-            type: "image_url",
-            image_url: {
-              url: `data:image/jpeg;base64,${base64Image}`,
-            },
-          },
-        ],
-      },
-    ],
-    max_tokens: 1024,
-  };
+        },
+      ],
+    },
+  ];
 
-  let endpoint: string = "";
-  let headers: HeadersInit = {};
+  let responseStream: Stream<ChatCompletionChunk> | undefined = undefined;
 
   if (get(useAzureOpenAI)) {
     if (
@@ -63,43 +66,53 @@ Please output the corrected text in a clean and readable format, ready for use. 
       return Promise.reject("Azure OpenAI parameters are not set.");
     }
 
-    endpoint = `${get(azureOpenAiEndpoint)}/openai/deployments/${get(
-      azureOpenAiDeployment
-    )}/chat/completions?api-version=${get(azureOpenAiVersion)}`;
+    let openai = new AzureOpenAI({
+      endpoint: get(azureOpenAiEndpoint),
+      apiKey: get(azureOpenAiKey),
+      apiVersion: get(azureOpenAiVersion),
+      deployment: get(azureOpenAiDeployment),
+      dangerouslyAllowBrowser: true,
+    });
 
-    headers = {
-      "api-key": get(azureOpenAiKey),
-      "Content-Type": "application/json",
-    };
+    responseStream = await openai.chat.completions.create(
+      {
+        model: get(azureOpenAiDeployment),
+        stream: true,
+        messages: messages,
+        max_tokens: 2048,
+      },
+      { maxRetries: 5, stream: true }
+    );
   } else {
     if (!get(openAiApiKey)) {
       toastMessage.set("OpenAI API key is not set.");
       return Promise.reject("OpenAI API key is not set.");
     }
 
-    endpoint = "https://api.openai.com/v1/chat/completions";
+    let openai = new OpenAI({
+      apiKey: get(openAiApiKey),
+      dangerouslyAllowBrowser: true,
+    });
 
-    headers = {
-      Authorization: `Bearer ${get(openAiApiKey)}`,
-      "Content-Type": "application/json",
-    };
+    responseStream = await openai.chat.completions.create(
+      {
+        model: "gpt-4-turbo",
+        messages: messages,
+        max_tokens: 2048,
+        stream: true,
+      },
+      { maxRetries: 5 }
+    );
   }
 
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(payload),
-  });
-
-  if (!response.ok) {
-    const errorBody = await response.text();
-    const errorMessage = `OpenAI GPT-4 Vision API call failed: ${response.statusText}, Body: ${errorBody}`;
-    toastMessage.set(errorMessage);
-    return Promise.reject(errorMessage);
+  let responseText = "";
+  for await (const chunk of responseStream) {
+    let newOutput = chunk.choices[0]?.delta?.content || "";
+    responseText += newOutput;
+    onStreamTokenCollaback(responseText, false);
   }
 
-  const data = await response.json();
-  return data.choices[0].message.content;
+  onStreamTokenCollaback(responseText, true);
 }
 
 function blobToBase64(blob: Blob): Promise<string> {
@@ -116,11 +129,11 @@ function blobToBase64(blob: Blob): Promise<string> {
 }
 
 export {
-  openAiApiKey,
-  useAzureOpenAI,
-  azureOpenAiEndpoint,
   azureOpenAiDeployment,
+  azureOpenAiEndpoint,
   azureOpenAiKey,
   azureOpenAiVersion,
   correctText,
+  openAiApiKey,
+  useAzureOpenAI,
 };
